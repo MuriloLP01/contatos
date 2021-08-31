9f126374491d8da339b881ff7aa5ca2f0df2e7de
Murilo Leão <murilo.leao.pereira@gmail.com>
2021-08-31 10:19:29 -0300

    Criado serviço de manipulação de contatos

--- src/app/components/lista-de-contatos/lista-de-contatos.component.ts
+++ src/app/components/lista-de-contatos/lista-de-contatos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Contato } from 'src/app/models/contato';
+import { ContatoService } from 'src/app/services/contato.service';
 
 @Component({
   selector: 'app-lista-de-contatos',
@@ -8,69 +9,12 @@ import { Contato } from 'src/app/models/contato';
 })
 export class ListaDeContatosComponent implements OnInit {
 
-  contatos:Contato[] = [
-    {
-      nome: "Mariana Ribeiro 1",
-      email: "mariana@teste.com",
-      telefones: ['1111-1111','2222-2222','3333-3333']
+  contatos: Contato[];
+  private cs:ContatoService = new ContatoService();
 
-    },
-    {
-      nome: "Mariana Ribeiro 2",
-      email: "mariana@teste.com",
-      telefones: ['1111-1111']
-
-    }, 
-    {
-      nome: "Mariana Ribeiro 3",
-      email: "mariana@teste.com",
-      telefones: ['1111-1111']
-
-    },    
-    {
-      nome: "Mariana Ribeiro 4",
-      email: "mariana@teste.com",
-      telefones: ['1111-1111']
-
-    },    
-    {
-      nome: "Mariana Ribeiro 5",
-      email: "mariana@teste.com",
-      telefones: ['1111-1111']
-
-    },    
-    {
-      nome: "Mariana Ribeiro 6",
-      email: "mariana@teste.com",
-      telefones: ['1111-1111']
-
-    },    
-    {
-      nome: "Mariana Ribeiro 7",
-      email: "mariana@teste.com",
-      telefones: ['1111-1111']
-
-    },    
-    {
-      nome: "Mariana Ribeiro 8",
-      email: "mariana@teste.com",
-      telefones: ['1111-1111']
-
-    },    
-    {
-      nome: "Mariana Ribeiro 9",
-      email: "mariana@teste.com",
-      telefones: ['1111-1111']
-
-    },    
-    {
-      nome: "Mariana Ribeiro 10",
-      email: "mariana@teste.com",
-      telefones: ['1111-1111']
-    }
-  ]
-
-  constructor() { }
+  constructor() { 
+    this.contatos = this.cs.getContatos()
+  }
 
   ngOnInit(): void {
   }
